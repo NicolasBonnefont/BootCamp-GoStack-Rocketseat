6b734f53b496d7fa8bc65fb8bd71cb48896fe7a5
Nicolas <nicolas.7l@hotmail.com>
2020-08-27 00:12:17 -0300

    Repositorio de todo BootCamp

---
+++ Nivel 1/FrontEnd/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+
+import api from './services/api'
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+
+  }, [])
+
+  async function handleAddProject() {
+
+    /* setProjects([...projects, `Novo Projetpo ${Date.now()}`]) */
+
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      Owner: "Nicolas"
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
+
+  }
+
+  return (
+    <>
+      <Header title="HomePage 2" />
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  )
+}
+
+export default App;
