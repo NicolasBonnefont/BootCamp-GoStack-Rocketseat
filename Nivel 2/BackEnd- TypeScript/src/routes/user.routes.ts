4f717e16176da18063cbd3069fb0104801ad692c
Nicolas <nicolas.7l@hotmail.com>
2020-09-01 19:55:07 -0300

    finalizacao do upload de arquivo

--- Nivel 2/BackEnd- TypeScript/src/routes/user.routes.ts
+++ Nivel 2/BackEnd- TypeScript/src/routes/user.routes.ts
@@ -1,15 +1,20 @@
 import { Router, request, response } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+import updateUserAvatarService from '../services/UpdateUserAvatarService'
 
 const usersRouter = Router()
-
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
 
   try {
 
-    const {name, email, password} =  request.body
+    const { name, email, password } = request.body
 
     const createUser = new CreateUserService()
 
@@ -21,12 +26,26 @@ usersRouter.post('/', async (request, response) => {
 
     delete user.password
 
-
     return response.json(user)
   } catch (err) {
     return response.status(400).json({ erro: err.message })
   }
 })
 
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  try {
+    const updateUserAvatar = new updateUserAvatarService()
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename:request.file.filename
+    })
+
+    delete user.password
+    return response.json(user)
+  } catch (err) {
+    return response.status(400).json({ erro: err.message })
+  }
+})
 
 export default usersRouter
